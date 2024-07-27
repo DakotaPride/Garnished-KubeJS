456adb01e377a9d72ff1c9e4cdd3bb5fe1778347
Dakota <rjtube13@gmail.com>
2024-07-27 10:44:59 -0500

    0.2 - update to Create: Garnished v1.9

--- src/main/java/net/dakotapride/garnished_kubejs/GarnishedKubejsPlugin.java
+++ src/main/java/net/dakotapride/garnished_kubejs/GarnishedKubejsPlugin.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 public class GarnishedKubejsPlugin extends KubeJSPlugin {
 
-    private static final Map<GarnishedRecipeTypes, RecipeSchema> recipeSchemas = Map.of(
+    private static final Map<GarnishedRecipeTypes, RecipeSchema> standardSchema = Map.of(
             GarnishedRecipeTypes.FREEZING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
             GarnishedRecipeTypes.RED_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
             GarnishedRecipeTypes.ORANGE_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
@@ -19,6 +19,19 @@ public class GarnishedKubejsPlugin extends KubeJSPlugin {
             GarnishedRecipeTypes.GREEN_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
             GarnishedRecipeTypes.BLUE_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
             GarnishedRecipeTypes.PURPLE_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING
+    );
+
+    private static final Map<GarnishedRecipeTypes, RecipeSchema> additionalSchema = Map.of(
+            GarnishedRecipeTypes.LIME_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
+            GarnishedRecipeTypes.LIGHT_BLUE_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
+            GarnishedRecipeTypes.CYAN_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
+            GarnishedRecipeTypes.MAGENTA_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
+            GarnishedRecipeTypes.PINK_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
+            GarnishedRecipeTypes.BLACK_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
+            GarnishedRecipeTypes.GRAY_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
+            GarnishedRecipeTypes.LIGHT_GRAY_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
+            GarnishedRecipeTypes.WHITE_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING,
+            GarnishedRecipeTypes.BROWN_DYE_BLOWING, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING
     );
 
     @Override
@@ -30,8 +43,10 @@ public class GarnishedKubejsPlugin extends KubeJSPlugin {
     public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
         for (var createRecipeType : GarnishedRecipeTypes.values()) {
             if (createRecipeType.getSerializer() instanceof ProcessingRecipeSerializer<?>) {
-                var schema = recipeSchemas.getOrDefault(createRecipeType, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING);
+                var schema = standardSchema.getOrDefault(createRecipeType, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING);
+                var schema1 = additionalSchema.getOrDefault(createRecipeType, BasicFanRecipeSchema.DEFAULT_BULK_PROCESSING);
                 event.register(createRecipeType.getId(), schema);
+                event.register(createRecipeType.getId(), schema1);
             }
         }
     }
