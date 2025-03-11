6c141825e1c894263863eec53a514f79d43a98b2
Dakota <rjtube13@gmail.com>
2025-03-10 19:34:03 -0500

    0.2.2 - update garnished + create mod update 6

--- src/main/java/net/dakotapride/garnished_kubejs/HatchetItemBuilder.java
+++ src/main/java/net/dakotapride/garnished_kubejs/HatchetItemBuilder.java
@@ -1,10 +1,7 @@
 package net.dakotapride.garnished_kubejs;
 
-import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
-import dev.latvian.mods.kubejs.item.ItemBuilder;
 import dev.latvian.mods.kubejs.item.custom.HandheldItemBuilder;
-import dev.latvian.mods.kubejs.item.custom.SwordItemBuilder;
 import dev.latvian.mods.kubejs.registry.RegistryInfo;
 import net.dakotapride.garnished.item.hatchet.HatchetToolItem;
 import net.minecraft.resources.ResourceLocation;
@@ -12,7 +9,6 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SwordItem;
 
 public class HatchetItemBuilder extends HandheldItemBuilder {
 
